fd61d2408d55c80b5a34908537fc473edf125853
Maaz <okkmaaz@gmail.com>
2026-03-30 04:58:53 +0500

    Setup Map Rendering + Added unordered map to track tileID labeling

--- map.cpp
+++ map.cpp
@@ -2,25 +2,36 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <unordered_map>
 
 class Map {
     private:
         sf::Texture texture;
-        sf::Sprite TileMap;
+        sf::Sprite currentTile;
 
         // Combined 2D Vector Map Size.
         int mapHeight = 45; // Map Rows
         int mapWidth = 80; // Map Columns
 
-        std::string fileloc = "tilemap/era1.png";
+        // Tilesheet Dimensions
         int tileHeight = 14;
         int tileWidth = 25;
         int tileSpacing = 16;
+
+        // File Location & Seeding Gen
         std::mt19937 rng;
+        std::string fileloc = "tilemap/era1.png";
+
+        // Mapping & Lookup
         std::uniform_int_distribution<int> dist;
         std::vector<std::vector<int>> vectorMap;
+        std::unordered_map<int, sf::Vector2i> tileLookup;
 
-    
+        // Tile Information
+        int tileID;
+        sf::Vector2i tilePos;
+
+
     private:
         // Random Number Generator (apparently widely used for game development)
         int randomTileGenerator() {
@@ -32,16 +43,19 @@ class Map {
         }
 
     public:
-        Map(): rng(std::random_device{}()), dist(0, 99) {Load();} 
+        Map(): rng(std::random_device{}()), dist(0, 99) {
+            Load();
+        } 
 
         void Load() {
             if (texture.loadFromFile(fileloc)) {
-                TileMap.setTexture(texture);
+                currentTile.setTexture(texture);
                 std::cout << "Loaded TileMap Successfully." << std::endl;
             } else {std::cout << "Failed To Load TileMap." << std::endl;}
 
 
-            // Resize Vector To Map
+            // Create Label Lookup Table & Resize Vector To Map 
+            createLookupTable();
             vectorMap.resize(mapHeight, std::vector<int>(mapWidth, 0));
 
             for (int i=0; i<mapHeight; i++) {
@@ -49,6 +63,15 @@ class Map {
                     vectorMap[i][j] = randomTileGenerator();
                 }
             }
+
+        }
+
+        void createLookupTable() {
+            // Managing Tiles
+            tileLookup[1] = {7, 5}; // Grass
+            tileLookup[2] = {12, 12}; // Bush
+            tileLookup[3] = {12, 16}; // Fern
+            tileLookup[4] = {14, 1}; // Rock
         }
 
         void displayVectorMap() {
@@ -61,8 +84,29 @@ class Map {
             std::cout << std::endl;
         }
 
+        void TileToMap(sf::Vector2i tilePos, int i, int j) {
+            int pX = tilePos.x*tileSpacing;
+            int pY = tilePos.y*tileSpacing;
+            currentTile.setTextureRect(sf::IntRect(pX, pY, tileSpacing,tileSpacing));
+
+            // Convert Map Position To Screen Position
+            currentTile.setPosition(
+                i * tileSpacing,
+                j * tileSpacing
+            );
+        }
+
         void Update() {}
-        void Draw() {}
+        void Draw(sf::RenderWindow& window) {
+            for (int i=0; i<mapHeight; i++) {
+                for (int j=0; j<mapWidth; j++) {
+                    tileID = vectorMap[i][j];
+                    tilePos = tileLookup[tileID];
+                    TileToMap(tilePos, i, j);
+                    window.draw(currentTile);
+                }
+            }
+        }
 };
 
 
