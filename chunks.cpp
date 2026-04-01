13f714b46b7c9a80cf06ea320c9c9fdbeb174919
Maaz <okkmaaz@gmail.com>
2026-04-01 05:54:32 +0500

    Created Chunk system, it is operational. Singular 16x16 chunks are rendered & pre-generated

--- chunks.cpp
+++ chunks.cpp
@@ -3,10 +3,23 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include <unordered_map>
 
 static constexpr int chunkSize = 16;
+static constexpr int tileSpacing = 16;
 std::mt19937 rng(std::random_device{}());
 std::uniform_int_distribution<int> dist(1, 4);
+std::unordered_map<int, sf::Vector2i> TileLookup;
+
+void createLookup() {
+    TileLookup[0] = {4, 6};
+    TileLookup[1] = {0, 0};
+    TileLookup[2] = {1, 0};
+    TileLookup[3] = {2, 0};
+    TileLookup[4] = {3, 0};
+}
+
+sf::Vector2i ConvertToPos(int itemID) {return TileLookup.at(itemID);}
 
 class Chunk {
     public: 
@@ -20,7 +33,6 @@ class Chunk {
         }
 };
 
-
 // 0: Grass, 1: Anything else.
 void GenerateChunk(Chunk& chunk) {
     for (int i=0; i<chunkSize; i++) {
@@ -31,8 +43,32 @@ void GenerateChunk(Chunk& chunk) {
     }
 }
 
-void LoadChunk(const Chunk& chunk) {
-    
+
+
+void TileToChunk(sf::Sprite& currentTile, sf::Vector2i tilePos, int row, int col, int offsetX, int offsetY) {
+    int posX = tilePos.x*tileSpacing;;
+    int posY = tilePos.y*tileSpacing;
+    currentTile.setTextureRect({posX, posY, tileSpacing, tileSpacing});
+    currentTile.setPosition(offsetX + col*tileSpacing, offsetY + row*tileSpacing);
+}
+
+void RenderChunk(sf::RenderWindow& window, sf::Sprite& currentTile, const Chunk& chunk) {
+
+    // Centering
+    int chunkPixelSize = chunkSize * tileSpacing;
+    sf::Vector2u winSize = window.getSize();
+    float offsetX = (winSize.x - chunkPixelSize) / 2.f;
+    float offsetY = (winSize.y - chunkPixelSize) / 2.f;
+    // ---------
+
+    for (int i=0; i<chunkSize; i++) {
+        for (int j=0; j<chunkSize; j++) {
+            int ID = chunk.vectorMapGround[i][j];
+            sf::Vector2i tilePos = ConvertToPos(ID);
+            TileToChunk(currentTile, tilePos, i, j, offsetX, offsetY);
+            window.draw(currentTile);
+        }
+    }
 }
 
 void DisplayChunk(const Chunk& chunk) {
@@ -53,21 +89,37 @@ void DisplayChunk(const Chunk& chunk) {
     }
 }
 
+void LoadTexture(sf::Sprite& currentTile, sf::Texture& texture) {
+    std::string fileloc="tilemap/era1.png";
+
+    if (texture.loadFromFile(fileloc)) {
+        currentTile.setTexture(texture);
+    }
+}
+
 int main() {
-    // sf::RenderWindow window;
-    // sf::Event event;
-    // window.create(sf::VideoMode(1280, 720), "The Immortal Snail");
-    // while (window.isOpen()) {
-    //     while (window.pollEvent(event)) {
-    //         if (event.type == sf::Event::Closed) {
-    //             window.close();
-    //         }
-    //     }
-    //     window.clear(sf::Color(30, 30, 30));
-    //     window.display();
-    // }
+    sf::RenderWindow window;
+    sf::Event event;
+    sf::Texture texture;
+    sf::Sprite currentTile;
+    LoadTexture(currentTile, texture);
+
     Chunk chunk;
     GenerateChunk(chunk);
-    DisplayChunk(chunk);
+    window.create(sf::VideoMode(1280, 720), "The Immortal Snail");
+
+    createLookup();
+
+    while (window.isOpen()) {
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        window.clear(sf::Color(30, 30, 30));
+        RenderChunk(window, currentTile, chunk);
+        window.display();
+    }
+
 
 }
