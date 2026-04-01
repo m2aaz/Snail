d436f0e46ce7306e11f0675e905ba44adc416fe1
Maaz <okkmaaz@gmail.com>
2026-04-01 07:30:41 +0500

    Created a camera, it now handles movement across the game window, also grabbed snippet of code for resizing with aspect ratio, so future player/entity does not ruin scale when window is expanded or collapsed

--- admin.cpp
+++ admin.cpp
@@ -3,6 +3,36 @@
 #include "chunks.cpp"
 #include <iostream>
 
+struct Camera {
+    public:
+        sf::View cam;
+        float speed=5.0f;
+
+    public:
+        void Init(sf::RenderWindow& window) {
+            cam = window.getDefaultView();
+            window.setView(cam);
+        }
+
+        void Update(sf::RenderWindow& window) {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+                cam.move(0, -speed);
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+                cam.move(0, speed);
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+                cam.move(-speed, 0);
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+                cam.move(speed, 0);
+
+            window.setView(cam);
+        }
+
+        void ResizeView(sf::RenderWindow& window, const int gameWidth, const int gameHeight) {
+            float aspectRatio = float(window.getSize().x)/float(window.getSize().y);
+            cam.setSize(gameWidth * aspectRatio, gameHeight);
+        }
+};
+
 class Admin {
     private:
         const int gameWidth = 1280;
@@ -14,10 +44,13 @@ class Admin {
         sf::Event event;
         Chunk newChunk;
         ChunkRenderer Renderer;
+        Camera cam;
 
 
     private:
-        void Update() {}
+        void Update() {
+            cam.Update(window);
+        }
         void Draw() {
             window.clear(sf::Color(30, 30, 30));
             Renderer.RenderChunk(window, newChunk);
@@ -38,6 +71,9 @@ class Admin {
             // Set Window Frames.
             delta.Init();
             delta.setFrameRate(window, 60);
+
+            // Initialise Camera
+            cam.Init(window);
         }
 
         void Run() {
@@ -52,6 +88,8 @@ class Admin {
         void Exit() {
             // Handle Event (Closing)
             while (window.pollEvent(event)) {
+                if (event.type == sf::Event::Resized) 
+                {cam.ResizeView(window, gameWidth, gameHeight);}
                 if (event.type == sf::Event::Closed) 
                 {window.close(); std::cout << "Window Closed." << std::endl;}
             }
