5032958627ff1b8edb1e622b7f3de9c88043d424
Maaz <okkmaaz@gmail.com>
2026-04-11 16:37:31 +0500

    Fixed FPS rendering in the middle of the screen

--- src/clock.cpp
+++ src/clock.cpp
@@ -8,6 +8,7 @@ void Clock::Init() {
 	if (dispFPSFont.loadFromFile("fonts/Arial.ttf")) {
 		dispFPS.setFont(dispFPSFont);
 		dispFPS.setScale(sf::Vector2f(0.5f, 0.5f));
+		dispFPS.setPosition(0, 0);
 	} else {
 		std::cout << "Warning: Could not load fonts/Arial.ttf" << std::endl;
 	}
@@ -25,9 +26,12 @@ void Clock::setFrameRate(sf::RenderWindow &window, int fps) {
 void Clock::displayFPS(sf::RenderWindow &window) {
 	if (display) {
 		// Static cast to int to avoid trailing 0's with float->string
-		// conversion
-		dispFPS.setString("FPS: " +
-						  std::to_string(int(round(1000 / deltaTime))));
+		dispFPS.setString("FPS: " +std::to_string(int(round(1000 / deltaTime))));
+
+		// Conserve old view to render FPS object at top left without camera distorting it's position
+		sf::View oldView = window.getView();
+		window.setView(window.getDefaultView());
 		window.draw(dispFPS);
+		window.setView(oldView);
 	}
 }
